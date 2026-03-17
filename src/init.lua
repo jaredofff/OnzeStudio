a318e2c187e9d54b4f438a9eaf413fe06c53f012
jaredofff <sierraeuanjared@republicademexico.edu.mx>
2026-03-16 22:08:17 -0600

    Add linter suppressions

--- src/init.lua
+++ src/init.lua
@@ -4,6 +4,9 @@
     Version: 1.0.0
 ]]
 
+--!nocheck
+---@diagnostic disable: undefined-global
+
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
